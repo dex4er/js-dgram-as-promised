bcded10163ba281dcf825efd2d7cecd402fa3597
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-02-18 21:42:34 +0100

    ES7

--- examples/send-multicast.js
+++ examples/send-multicast.js
@@ -4,22 +4,26 @@ const dgramAsPromised = require('dgram-as-promised')
 
 const socket = dgramAsPromised.createSocket('udp4')
 
-const membership = '224.0.0.1'
-const port = 41234
+const MEMBERSHIP = '224.0.0.1'
+const PORT = 41234
 
 const message = new Buffer('ABCDEFGH')
 
-socket.bind().then(() => {
+async function main () {
+  await socket.bind()
   console.log('Socket is listening')
+
   socket.setBroadcast(true)
   socket.setMulticastTTL(128)
-  socket.addMembership(membership)
-}).then(() => {
+
+  await socket.addMembership(MEMBERSHIP)
   console.log('Membership is set')
-  return socket.send(message, 0, message.length, port, membership)
-}).then(() => {
+
+  await socket.send(message, 0, message.length, PORT, MEMBERSHIP)
   console.log('Message is sent')
-  return socket.close()
-}).then(() => {
+
+  await socket.close()
   console.log('Socket is closed')
-})
+}
+
+main()
