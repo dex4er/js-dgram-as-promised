f3a0dfeb254dc8976bd806e968d1f4019921d526
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-02-18 21:42:27 +0100

    ES6

--- test/dgram-as-promised.js
+++ test/dgram-as-promised.js
@@ -1,21 +1,21 @@
 'use strict'
 
-var t = require('tap')
-var dgramAsPromised = require('../lib/dgram-as-promised')
+const t = require('tap')
+const dgramAsPromised = require('../lib/dgram-as-promised')
 
 var TIMEOUT = 10000
 
-t.plan(1)
+t.plan(2)
 
-t.test('Send datagram', {timeout: TIMEOUT}, function (t) {
+t.test('Send datagram', {timeout: TIMEOUT}, t => {
   t.plan(5)
 
-  var socket = dgramAsPromised.createSocket('udp4')
+  const socket = dgramAsPromised.createSocket('udp4')
 
-  var promise = socket.bind()
-  promise.then(function () {
+  const promise = socket.bind()
+  promise.then(() => {
     t.pass('socket.bind is fulfilled')
-    var address = '224.0.0.1'
+    let address = '224.0.0.1'
     try {
       socket.setBroadcast(true)
       socket.setMulticastTTL(128)
@@ -24,17 +24,31 @@ t.test('Send datagram', {timeout: TIMEOUT}, function (t) {
       address = '127.0.0.1'
     }
     return socket.send(new Buffer('ABCDEFGH'), 0, 8, 41234, address)
-  }).then(function () {
+  }).then(() => {
     t.pass('socket.send is fulfilled')
     return socket.send('', 0, 0, null, null)
   }).catch(function (err) {
     t.type(err, 'Error', 'socket.send is rejected: ' + err)
     return socket.close()
-  }).then(function () {
+  }).then(() => {
     t.pass('socket.close is fulfilled')
     return socket.close()
   }).catch(function (err) {
     t.type(err, 'Error', 'socket.close is rejected: ' + err)
     t.end()
+  })
+})
+
+t.test('createSocket with callback for message signal', {timeout: TIMEOUT}, t => {
+  t.plan(2)
+
+  const socket = dgramAsPromised.createSocket('udp4', (msg, rinfo) => {
+    t.pass('message signal received')
+    t.equals(msg, 'It-s-a-me', 'message is correct')
+  })
+
+  const promise = socket.bind()
+  promise.then(() => {
+    t.pass('socket.bind is fulfilled')
   })
 })
