ba71eeb0ae54083c5b97350ae06e8cdd3281bee0
Piotr Roszatycki <piotr.roszatycki@gmail.com>
2017-02-18 22:59:22 +0100

    BDD tests

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## v0.1.0 2017-02-18
 
   * ES6
+  * BDD tests
   * Relicensed to MIT
 
 ## v0.0.2 2016-07-31
--- test/dgram-as-promised.js
+++ test/dgram-as-promised.js
@@ -1,54 +1,87 @@
 'use strict'
 
+/* global scenario, given, when, then, after */
 const t = require('tap')
+require('tap-given')(t)
+require('chai').should()
+
 const dgramAsPromised = require('../lib/dgram-as-promised')
 
-var TIMEOUT = 10000
+scenario('Send datagram', function () {
+  given('socket', () => {
+    this.socket = dgramAsPromised.createSocket('udp4')
+    return this.socket
+  })
 
-t.plan(2)
+  when('socket is bound', () => {
+    return this.socket.bind()
+  })
 
-t.test('Send datagram', {timeout: TIMEOUT}, t => {
-  t.plan(5)
-
-  const socket = dgramAsPromised.createSocket('udp4')
-
-  const promise = socket.bind()
-  promise.then(() => {
-    t.pass('socket.bind is fulfilled')
-    let address = '224.0.0.1'
+  when('membership is added', () => {
     try {
-      socket.setBroadcast(true)
-      socket.setMulticastTTL(128)
-      socket.addMembership(address)
-    } catch (err) {
-      address = '127.0.0.1'
+      this.address = '224.0.0.1'
+      this.socket.setBroadcast(true)
+      this.socket.setMulticastTTL(128)
+      this.socket.addMembership(this.address)
+    } catch (e) {
+      this.address = '127.0.0.1'
     }
-    return socket.send(new Buffer('ABCDEFGH'), 0, 8, 41234, address)
-  }).then(() => {
-    t.pass('socket.send is fulfilled')
-    return socket.send('', 0, 0, null, null)
-  }).catch(function (err) {
-    t.type(err, 'Error', 'socket.send is rejected: ' + err)
-    return socket.close()
-  }).then(() => {
-    t.pass('socket.close is fulfilled')
-    return socket.close()
-  }).catch(function (err) {
-    t.type(err, 'Error', 'socket.close is rejected: ' + err)
-    t.end()
+  })
+
+  when('correct message is sent', () => {
+    return this.socket.send(new Buffer('ABCDEFGH'), 0, 8, 41234, this.address)
+  })
+
+  when('socket is closed', () => {
+    return this.socket.close()
+  })
+
+  when('I try to close again', () => {
+    return this.socket.close()
+    .catch(e => {
+      this.error = e
+    })
+  })
+
+  then("can't be closed again", () => {
+    this.error.should.be.an('Error')
   })
 })
 
-t.test('createSocket with callback for message signal', {timeout: TIMEOUT}, t => {
-  t.plan(2)
-
-  const socket = dgramAsPromised.createSocket('udp4', (msg, rinfo) => {
-    t.pass('message signal received')
-    t.equals(msg, 'It-s-a-me', 'message is correct')
+scenario("Can't send datagram", function () {
+  given('socket', () => {
+    this.socket = dgramAsPromised.createSocket('udp4')
+    return this.socket
   })
 
-  const promise = socket.bind()
-  promise.then(() => {
-    t.pass('socket.bind is fulfilled')
+  when('socket is bound', () => {
+    return this.socket.bind()
+  })
+
+  when('membership is added', () => {
+    try {
+      this.address = '224.0.0.1'
+      this.socket.setBroadcast(true)
+      this.socket.setMulticastTTL(128)
+      this.socket.addMembership(this.address)
+    } catch (e) {
+      this.address = '127.0.0.1'
+    }
+  })
+
+  when('wrong message is sent', () => {
+    return this.socket.send('', 0, 0, null, null)
+    .catch(e => {
+      this.error = e
+    })
+  })
+
+  then("can't be sent", () => {
+    this.error.should.be.an('Error')
+  })
+
+  after('close the socket', () => {
+    return this.socket.close()
+    .catch(e => {})
   })
 })
